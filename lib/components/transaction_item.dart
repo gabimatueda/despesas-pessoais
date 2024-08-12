2ff62b4ef8cf23538617871fe488afb289f30b7e
Gabriela <gabriela4matueda@gmail.com>
2024-08-12 14:12:53 -0300

    primeiro commit

---
+++ lib/components/transaction_item.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import '../models/transaction.dart';
+import 'package:intl/intl.dart';
+
+class TransactionItem extends StatelessWidget {
+  const TransactionItem({
+    Key? key,
+    required this.tr,
+    required this.onRemove,
+  }) : super(key: key);
+
+  final Transaction tr;
+  final void Function(String p1) onRemove;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      margin: const EdgeInsets.symmetric(
+        vertical: 8,
+        horizontal: 5,
+      ),
+      child: ListTile(
+        leading: CircleAvatar(
+          backgroundColor: Colors.purple,
+          radius: 30,
+          child: Padding(
+            padding: const EdgeInsets.all(6),
+            child: FittedBox(
+              child: Text(
+                'R\$${tr.value}',
+                style: const TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        title: Text(
+          tr.title,
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+        subtitle: Text(
+          DateFormat('d MMM y').format(tr.date),
+        ),
+        trailing: MediaQuery.of(context).size.width > 480
+            ? TextButton.icon(
+                onPressed: () => onRemove(tr.id),
+                icon: const Icon(Icons.delete, color: Colors.red),
+                label: const Text(
+                  'Excluir',
+                  style: TextStyle(color: Colors.red),
+                ),
+              )
+            : IconButton(
+                icon: const Icon(Icons.delete),
+                color: Colors.red,
+                onPressed: () => onRemove(tr.id),
+              ),
+      ),
+    );
+  }
+}
